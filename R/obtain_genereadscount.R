c7f83ebbe581bb492443b382cf544ae07235e639
NWPU-903PR <37072138+NWPU-903PR@users.noreply.github.com>
2021-10-23 11:34:57 +0800

    Create obtain_genereadscount.R

---
+++ R/obtain_genereadscount.R
@@ -0,0 +1,10 @@
+library(Rsubread)
+obtain_gene_readscount <- function(Input_data,annotation_file,isPairedEnd=F){
+  gene_count <- featureCounts(Input_data,isGTFAnnotationFile=TRUE,GTF.featureType="exon",GTF.attrType="gene_name", 
+                              annot.ext = annotation_file, isPairedEnd=F)
+  gene_readscount <- as.data.frame(gene_count$counts)
+  gene_names <- as.character(rownames(gene_readscount))
+  gene_readscount <- data.frame(gene_name=gene_names,gene_readscount)
+  rownames(gene_readscount) <- NULL
+  return(gene_readscount)
+}
