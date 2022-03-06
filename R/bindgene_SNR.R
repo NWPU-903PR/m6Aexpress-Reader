97ee693f4ceca7afee7a90dc2cdda7c04972b82c
NWPU-903PR <37072138+NWPU-903PR@users.noreply.github.com>
2022-03-06 17:21:06 +0800

    Update and rename add_bindgene_SNR.R to bindgene_SNR.R

--- R/bindgene_SNR.R
+++ R/bindgene_SNR.R
@@ -2,7 +2,7 @@
 bindgene_SNR <- function(bindgene_peak_SNR_infor){
   bindgene_bindallsites_addSNRinfor <- bindgene_peak_SNR_infor$onlybindgene_bindsites_addSNRinfor
   bindgene_bindpartsites_addSNRinfor <- bindgene_peak_SNR_infor$bindgene_distsites_addSNRinfor
-  bindgene_SNR <- function(bindgene_infor, gene_name){
+  .bindgeneSNR <- function(bindgene_infor, gene_name){
     bindgenes_SNR <-data.frame()
     for (i in 1:length(gene_name)) {
       onebindgene_SNR <- log(sum(exp(bindgene_infor[bindgene_infor$gene_name==gene_name[i],]$SNR)))
@@ -11,10 +11,10 @@ bindgene_SNR <- function(bindgene_peak_SNR_infor){
     }
     return(bindgenes_SNR)
   }
-  bindgene_bindallsites_SNR <- bindgene_SNR(bindgene_infor=bindgene_bindallsites_addSNRinfor,
+  bindgene_bindallsites_SNR <- .bindgeneSNR(bindgene_infor=bindgene_bindallsites_addSNRinfor,
                                             gene_name = unique(as.character(bindgene_bindallsites_addSNRinfor$gene_name)))
   
-  bindgene_partbind_SNR <- bindgene_SNR(bindgene_infor=bindgene_bindpartsites_addSNRinfor,
+  bindgene_partbind_SNR <- .bindgeneSNR(bindgene_infor=bindgene_bindpartsites_addSNRinfor,
                                         gene_name = unique(as.character(bindgene_bindpartsites_addSNRinfor$gene_name)))
   bindgenes_SNR <- data.frame(rbind(bindgene_bindallsites_SNR,bindgene_partbind_SNR),
                               bind_case=c(rep("bind_allsites",nrow(bindgene_bindallsites_SNR)),
